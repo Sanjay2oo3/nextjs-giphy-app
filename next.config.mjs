d19de6601c73f7ed0be78011a93c024948109cb4
Sanjay Choudhary <itssanjaychoudhary423@gmail.com>
2024-08-26 01:48:54 +0530

    Given env vairable directly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/nextjs-giphy-app' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-giphy-app/' : '',
+  };
+  
+  export default nextConfig;
+  
--- utils/firebase.js
+++ utils/firebase.js
@@ -3,12 +3,10 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAjLgh_4W2cll4zPHDX-pmVkQxyTTNToWY",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "nextjs-giphy-app.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "nextjs-giphy-app", 
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:986654822886:web:e844237a315bb721e58627"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
